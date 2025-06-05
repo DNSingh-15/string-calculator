323d4d070a6d81702b62f78947b8279401df7c05
D.N.Singh <asmrdnsingh@gmail.com>
2025-06-05 21:42:11 +0530

    Two Numbers Test

--- src/string-calculator.test.ts
+++ src/string-calculator.test.ts
@@ -4,4 +4,7 @@ describe('String Calculator', () => {
     it('should return 0 for empty string', () => {
         expect(add('')).toBe(0);
     });
+    it('should return the number itself when single number is passed', () => {
+        expect(add('1')).toBe(1);
+    });
 });
